0f00ef50bc947654db89d016b49dbf481c383957
jam.hamidi <jam.hamidi@maximusbc.ca>
2017-12-27 11:42:15 -0800

    work in progress

--- index.js
+++ index.js
@@ -7,93 +7,132 @@ var app = require('express')();
 var SplunkLogger = require("./splunklogger");
 var utils = require("./utils");
 
-// where to listen
+// configuration via environment variables
 var SERVICE_IP = process.env.SERVICE_IP || '0.0.0.0';
 var SERVICE_PORT = process.env.SERVICE_PORT || 5040;
-
-// log level for this service
-var LOG_LEVEL = process.env.LOG_LEVEL || "debug";
-
-// Generate token for Splunk
-// -- TBD if this can be used by the server
-if (process.env.USE_AUTH_TOKEN &&
-    process.env.USE_AUTH_TOKEN == "true" &&
-    process.env.AUTH_TOKEN_KEY &&
-    process.env.AUTH_TOKEN_KEY.length > 0) {
-
-    var signingToken = process.env.AUTH_TOKEN_KEY);
-    winston.debug("Signing token: " + signingToken);
+var SERVICE_AUTH_TOKEN = 'NO_TOKEN';
+if (process.env.SERVICE_USE_AUTH &&
+    process.env.SERVICE_USE_AUTH == 'true' &&
+    process.env.SERVICE_AUTH_TOKEN &&
+    process.env.SERVICE_AUTH_TOKEN.length > 0) {
+        SERVICE_AUTH_TOKEN = process.env.SERVICE_AUTH_TOKEN;
+}
+var FILE_LOG_LEVEL = process.env.FILE_LOG_LEVEL || 'debug';
+var FILE_LOG_NAME = process.env.FILE_LOG_NAME || './msp.log';
+var USE_SPLUNK = false;
+var SPLUNK_URL = 'NO_SPLUNK';
+if (process.env.USE_SPLUNK &&
+    process.env.USE_SPLUNL == 'true' &&
+    process.env.SPLUNK_URL &&
+    process.env.SPLUNK_URL.length > 0) {
+        USE_SPLUNK = true;
+        SPLUNK_URL = process.env.SPLUNK_SERVER_URL;
 }
 
-// Logger init
-winston.level = LOG_LEVEL;
-winston.remove(winston.transports.Console);
-winston.add(winston.transports.Console, {'timestamp': true});
+// Winston Logger init
+var winstonLogger = winston.createLogger({
+    level: LOG_LEVEL,
+    format: winston.format.json(),
+    transport: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: FILE_LOG_NAME, level: FILE_LOG_LEVEL })
+    ]
+});
 
-// Config logger
-var config = {
-   token = signingToken,
-   url = process.env.SPLUNK_SERVER_URL
-};
+// add timestamp, remove console if not in debug mode
+winston.add(winston.transport.File, {'timestamp': true});
+if (FILE_LOG_LEVEL != 'debug')
+    winston.remove(winston.transports.Console);
+else
+    winston.add(winston.transports.Console, {'timestamp': true});
 
-// Create a new logger
-var Logger = new SplunkLogger(config);
-Logger.error = function(err, context) {
-   winston.error(`SplunkLogger error:` + err + `  context:` + context);
-};
+// Create a new Splunk logger
+if (USE_SPLUNK) {
+    var config = {
+        token: SERVICE_AUTH_TOKEN,
+        url: SPLUNK_URL
+    };
+    var splunkLogger = new SplunkLogger(config);
+    Logger.error = function (err, context) {
+        winstonLogger.error(`SplunkLogger error:` + err + `  context:` + context);
+    };
+    splunkLogger.requestOptions.strictSSL = true;
+}
 
 // Init app
+if (process.env.NODE_ENV != 'production' ||
+    process.env.CORS_ALLOW_ALL == 'true') {
+    app.use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+}
 app.use(bodyParser.json());
 var args = process.argv;
 if (args.length == 3 && args[2] == 'server') {
-    var server = app.listen(SERVICE_PORT, SERVICE_IP, function () {
+    var server = app.listen(SERVICE_PORT, SERVICE_IP, function() {
         var host = server.address().address;
         var port = server.address().port;
-        winston.info(`MyGov Captcha Service listening at http://${host}:${port}`);
-        winston.info(`Log level is at: ${LOG_LEVEL}`);
+        winstonLogger.info(`MyGov Captcha Service listening at http://${host}:${port}`);
+        winstonLogger.info(`File location is: ${FILE_LOG_NAME}`);
+        winstonLogger.info(`File log level is: ${FILE_LOG_LEVEL}`);
     });
 }
 
 // get a log
-var getLog = function (logPayload) {
-   winston.debug(`getLog: ${logPayload}`);
+var getLog = function (logPayload, authorization) {
 
-   return new Promise(function (resolve, reject) {
+    var responseBody = 'SUCCESS';
+    if (SERVICE_AUTH_TOKEN) {
+        if (authorization != `Basic ${SERVICE_AUTH_TOKEN}`) {
+            responseBody = 'UNAUTHORIZED';
+            resolve({valid: false});
+            return responseBody;
+        }
+    }
 
-      var payload = {
-         message: {
-            log: logPayload
-         },
-         metadata: {
-            sourceIP: "TBD",
-            browserType: "TBD",
-            etc: "TBD"
-         },
-         severity: "info"
-      };
-      winston.debug(`sending payload`);
-      Logger.send(payload, function(err, resp, body) {
-         winston.log("Response from Splunk Server", body);
-      });
- 
-      // create basic response
-      var responseBody = {
-	  // TBD
-      };
-      resolve(responseBody);
-   }, function (err) {
-      winston.error(err);
-      resolve({valid: false});
-   });
+    // log to file system
+    winstonLogger.info(`getLog: ${logPayload}`);
+
+    if (USE_SPLUNK) {
+        return new Promise(function (resolve, reject) {
+
+            var payload = {
+                message: {
+                    log: logPayload
+                },
+                metadata: {
+                    sourceIP: "TBD",
+                    browserType: "TBD",
+                    etc: "TBD"
+                },
+                severity: "info"
+            };
+            winstonLogger.debug(`sending payload`);
+            splunkLogger.send(payload, function (err, resp, body) {
+                winstonLogger.debug("Response from Splunk Server", body);
+            });
+            resolve({valid: true});
+
+        }, function (err) {
+            responseBody = 'FAILURE';
+            winstonLogger.error(err);
+            resolve({valid: false});
+        });
+    }
+    else {
+        return responseBody;
+    }
 };
 exports.getLog = getLog;
 
 app.post('/log', function (req, res) {
-   getLog(req.body)
-      .then(function (body) {
-          winston.debug(`returning: OK`);
-          return res.send(responseBody);
-      });
+    getLog(req.body, req.get('Authorization'))
+        .then(function (body) {
+            winstonLogger.debug(`returning: ` + responseBody);
+            return res.send(responseBody);
+        });
 });
 
-winston.info(`Splunk Forwarder started on host:` +  SERVER_IP + `  port: ` + SERVER_PORT.green.bold);
+winstonLogger.info(`Splunk Forwarder started on host:` +  SERVER_IP + `  port: ` + SERVER_PORT.green.bold);
